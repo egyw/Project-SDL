cde9d525d63b66d3559d7a68c1ae3b3cfc0e8eb6
Hans Christian <152836345+hansckk@users.noreply.github.com>
2024-12-28 17:28:08 +0700

    persistent sg bener iki

---
+++ sdl_persistent_stack/sdl_persistent_stack/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace sdl_persistent_stack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.undoBtn = new System.Windows.Forms.Button();
+            this.redoBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 76);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 362);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // undoBtn
+            // 
+            this.undoBtn.Location = new System.Drawing.Point(598, 12);
+            this.undoBtn.Name = "undoBtn";
+            this.undoBtn.Size = new System.Drawing.Size(92, 58);
+            this.undoBtn.TabIndex = 1;
+            this.undoBtn.Text = "UNDO";
+            this.undoBtn.UseVisualStyleBackColor = true;
+            this.undoBtn.Click += new System.EventHandler(this.undoBtn_Click);
+            // 
+            // redoBtn
+            // 
+            this.redoBtn.Location = new System.Drawing.Point(696, 12);
+            this.redoBtn.Name = "redoBtn";
+            this.redoBtn.Size = new System.Drawing.Size(92, 58);
+            this.redoBtn.TabIndex = 2;
+            this.redoBtn.Text = "REDO";
+            this.redoBtn.UseVisualStyleBackColor = true;
+            this.redoBtn.Click += new System.EventHandler(this.redoBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.redoBtn);
+            this.Controls.Add(this.undoBtn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button undoBtn;
+        private System.Windows.Forms.Button redoBtn;
+    }
+}
+
---
+++ sdl_persistent_stack/sdl_persistent_stack/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sdl_persistent_stack
+{
+    public partial class Form1 : Form
+    {
+        private PersistentStack<undoRedoOperation> stack;
+        private int currentPosition;
+        private bool isUndoRedo = false;
+        private string lastText = "";
+        public Form1()
+        {
+            stack = new PersistentStack<undoRedoOperation>();
+            currentPosition = 0;
+            InitializeComponent();
+        }
+
+        private class undoRedoOperation
+        {
+            public readonly string Text;
+            public readonly int SelectionStart;
+            public readonly int SelectionLength;
+
+            public undoRedoOperation(string text, int selectionStart, int selectionLength)
+            {
+                Text = text;
+                SelectionStart = selectionStart;
+                SelectionLength = selectionLength;
+
+            }
+        }
+
+        private class PersistentStack<T>
+        {
+            private class Node
+            {
+                public readonly T Value;
+                public readonly Node Next;
+                public readonly int Size;
+
+                public Node(T value, Node next)
+                {
+                    Value = value;
+                    Next = next;
+                    Size = (next == null) ? 1 : next.Size + 1;
+                }
+            }
+            private readonly Node head;
+
+            public PersistentStack()
+            {
+                head = null;
+            }
+
+            private PersistentStack(Node head)
+            {
+                this.head = head;
+            }
+
+            public PersistentStack<T> Push(T value)
+            {
+                return new PersistentStack<T>(new Node(value, head));
+            }
+
+            public T Peek(int posistion)
+            {
+                Node current = head;
+                for (int i = 0; i < posistion && current != null; i++)
+                {
+                    current = current.Next;
+                }
+                return current != null ? current.Value : default(T);
+            }
+
+            public int Count
+            {
+                get { return head?.Size ?? 0; }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!isUndoRedo)
+            {
+                undoRedoOperation operation = new undoRedoOperation(lastText, textBox1.SelectionStart, textBox1.SelectionLength);
+                stack = stack.Push(operation);
+                currentPosition = 0;
+            }
+            lastText = textBox1.Text;
+        }
+
+        private void undo()
+        {
+            if(currentPosition + 1 < stack.Count)
+            {
+                isUndoRedo = true;
+
+                currentPosition++;
+                undoRedoOperation operation = stack.Peek(currentPosition);
+                if(operation != null)
+                {
+                    textBox1.Text = operation.Text;
+                    textBox1.SelectionStart = operation.SelectionStart;
+                    textBox1.SelectionLength = operation.SelectionLength;
+                }
+                isUndoRedo = false;
+                
+            }
+        }
+
+        private void redo()
+        {
+            if(currentPosition > 0)
+            {
+                isUndoRedo = true;
+                currentPosition--;
+                undoRedoOperation operation = stack.Peek(currentPosition);
+                if(operation != null)
+                {
+                    textBox1.Text = operation.Text;
+                    textBox1.SelectionStart = operation.SelectionStart;
+                    textBox1.SelectionLength = operation.SelectionLength;
+                }
+                isUndoRedo = false;
+            }
+        }
+        private void undoBtn_Click(object sender, EventArgs e)
+        {
+            undo();
+        }
+
+        private void redoBtn_Click(object sender, EventArgs e)
+        {
+            redo();
+        }
+    }
+}
